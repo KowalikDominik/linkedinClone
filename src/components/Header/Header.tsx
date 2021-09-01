957a97ba32be7f19e4d5e8e9bce520ec4acc37aa
Dominik <domi.kowalik@gmail.com>
2021-09-01 15:57:26 +0200

    Change navbar in header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button } from "react-bootstrap";
+import { Button, Container, Nav, Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 import { Logo } from "../Logo/Logo";
@@ -7,24 +7,33 @@ import "./Header.scss";
 
 export const Header: React.FC = () => {
   return (
-    <nav className="header max-width">
-      <Link to="/">
-        <Logo />
-      </Link>
-      <div className="options">
-        <Link className="option" to="/join">
-          <Button variant="outline-secondary" className="join">
-            Dołącz teraz
-          </Button>
-        </Link>
-        <Link className="option" to="/">
-          <Button variant="outline-primary" className="login">
-            Zaloguj się
-          </Button>
-        </Link>
-        <div className="option"></div>
-        <div className="option"></div>
-      </div>
-    </nav>
+    <Navbar collapseOnSelect expand="lg" className="header">
+      <Container>
+        <Navbar.Brand>
+          <Link to="/">
+            <Logo />
+          </Link>
+        </Navbar.Brand>
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto"></Nav>
+          <Nav>
+            <Nav.Link>
+              <Link className="option" to="/join">
+                <Button variant="outline-secondary" className="join">
+                  Dołącz teraz
+                </Button>
+              </Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link className="option" to="/">
+                <Button variant="outline-primary" className="login">
+                  Zaloguj się
+                </Button>
+              </Link>
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
